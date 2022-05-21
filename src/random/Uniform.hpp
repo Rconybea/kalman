8a86fd77fa483f06f64f3e26b9dfc48113a39b17
Roland Conybeare <rconybeare@gmail.com>
2022-05-21 12:02:18 -0400

    random: supply rng engine for uniform dist

--- src/random/Uniform.hpp
+++ src/random/Uniform.hpp
@@ -9,13 +9,15 @@ namespace xo {
   namespace random {
     // Generate pseudorandom numbers uniformly on [0.0, 1.0]
     //
+    template<class Engine>
     class UnitIntervalGen {
     public:
-      using generator_type = Generator<std::mt19937,
+      using engine_type = Engine;
+      using generator_type = Generator<Engine,
 				       std::uniform_real_distribution<double>>;
 
       static generator_type make(uint64_t seed) {
-	return generator_type::make(std::mt19937(seed),
+	return generator_type::make(Engine(seed),
 				    std::uniform_real_distribution<double>(0.0, 1.0));
       }
     }; /*UnitIntervalGen*/
