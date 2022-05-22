18aff8c1cd5de0114170a2241b8e58c38e8b2e16
Roland Conybeare <rconybeare@gmail.com>
2022-05-22 11:36:31 -0400

    logutil: add pad() inserter

---
+++ src/logutil/pad.hpp
@@ -0,0 +1,32 @@
+/* @file pad.hpp */
+
+#pragma once
+
+#include <iostream>
+
+namespace logutil {
+  class pad_impl {
+  public:
+    pad_impl(int32_t n) : n_pad_(n) {}
+
+    uint32_t n_pad() const { return n_pad_; }
+
+  private:
+    uint32_t n_pad_ = 0;
+  }; /*pad_impl*/
+
+  inline pad_impl
+  pad(uint32_t n) { return pad_impl(n); }
+
+  inline std::ostream &
+  operator<<(std::ostream &s,
+	     pad_impl const &pad)
+  {
+    for(uint32_t i=0; i<pad.n_pad(); ++i)
+      s << " ";
+    return s;
+  } /*operator<<*/
+
+} /*namespace logutil*/
+
+/* end pad.hpp */
