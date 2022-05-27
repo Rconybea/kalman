f9226b16b1a175df00b944aeef5c84e9f28559c0
Roland Conybeare <rconybeare@gmail.com>
2022-05-27 15:21:57 -0400

    rbtree: extend unit tests

--- src/tree/utest/redblacktree.cpp
+++ src/tree/utest/redblacktree.cpp
@@ -3,6 +3,7 @@
 #include "tree/RedBlackTree.hpp"
 #include "random/xoshiro256.hpp"
 #include "catch2/catch.hpp"
+#include <map>
 
 using xo::tree::RedBlackTree;
 using xo::tree::NullReduce;
@@ -45,9 +46,11 @@ random_inserts(uint32_t n,
 
   /* Require:
    * - rbtree has keys [0..n-1], where n=rbtree.size()
+   * - rbtree values at key k is dvalue+10*k
    */
   void
-  check_bidirectional_iterator(RedBlackTree<int, double, NullReduce<int>> const & rbtree)
+  check_bidirectional_iterator(uint32_t dvalue,
+			       RedBlackTree<int, double, NullReduce<int>> const & rbtree)
   {
     size_t const n = rbtree.size();
     size_t i = 0;
@@ -76,11 +79,12 @@ random_inserts(uint32_t n,
 		   xtag("locn", ix.location()),
 		   xtag("node", ix.node()));
 
-      REQUIRE((*ix).first == i);
+      REQUIRE(ix->first == i);
+      REQUIRE(ix->second == dvalue + 10*i);
       if(i > 0) {
-	REQUIRE((*ix).first > last_key);
+	REQUIRE(ix->first > last_key);
       }
-      last_key = (*ix).first;
+      last_key = ix->first;
       ++i;
       ++ix;
 
@@ -111,14 +115,35 @@ random_inserts(uint32_t n,
     REQUIRE(i == 0);
   } /*check_bidirectional_iterator*/
 
+  /* generate vector with integers [0.. n-1] */
+  std::vector<uint32_t>
+  vector_upto(uint32_t n)
+  {
+    std::vector<uint32_t> u(n);
+    for(uint32_t i=0; i<n; ++i)
+      u[i] = i;
+
+    return u;
+  } /*vector_upto*/
+
+  std::map<uint32_t, uint32_t>
+  map_upto(uint32_t n)
+  {
+    std::map<uint32_t, uint32_t> m;
+    for(uint32_t i=0; i<n; ++i) {
+      m[i] = i;
+    }
+
+    return m;
+  } /*map_upto*/
+
   /* generate random permutation of integers [0.. n-1] */
   std::vector<uint32_t>
   random_permutation(uint32_t n,
 		     xoshiro256 * p_rgen)
   {
-    std::vector<uint32_t> u(n);
-    for(uint32_t i=0; i<n; ++i)
-      u[i] = i;
+    /* vector [0 .. n-1] */
+    std::vector<uint32_t> u = vector_upto(n);
 
     /* shuffle to get unpredictable permutation */
     std::shuffle(u.begin(), u.end(), *p_rgen);
@@ -147,12 +172,48 @@ random_inserts(uint32_t n,
 
       REQUIRE(rbtree[x] == x*10);
       REQUIRE(rbtree.verify_ok());
+      REQUIRE(rbtree.size() == n);
       ++i;
     }
 
     REQUIRE(rbtree.size() == n);
   } /*random_lookups*/
 
+  /* Require:
+   * - *p_rbtree has keys [0..n-1],  where n=rbtree.size()
+   * - for each key k,  associated value is 10*k
+   *
+   * Promise:
+   * - for each key k,  associated value is dvalue + 10*k
+   */
+  void
+  random_updates(uint32_t dvalue,
+		 RedBlackTree<int, double, NullReduce<int>> * p_rbtree,
+		 xoshiro256 * p_rgen)
+  {
+    REQUIRE(p_rbtree->verify_ok());
+
+    std::size_t n = p_rbtree->size();
+    std::vector<uint32_t> u
+      = random_permutation(n, p_rgen);
+
+    /* update key/value pairs in permutation order */
+    uint32_t i = 1;
+    for (uint32_t x : u) {
+      REQUIRE((*p_rbtree)[x] == x*10);
+
+      (*p_rbtree)[x] = dvalue + 10*x;
+
+      REQUIRE((*p_rbtree)[x] == dvalue + 10*x);
+      REQUIRE(p_rbtree->verify_ok());
+      /* assignment to existing key does not change tree size */
+      REQUIRE(p_rbtree->size() == n);
+      ++i;
+    }
+
+    REQUIRE(p_rbtree->size() == n);
+  } /*random_updates_1*/
+  
   /* Require:
    * - *p_rbtree has keys [0..n-1], where n=p_rbtree->size()
    */
@@ -162,20 +223,51 @@ random_inserts(uint32_t n,
   {
     REQUIRE(p_rbtree->verify_ok());
 
+    uint32_t n = p_rbtree->size();
+
     /* random permutation of keys in *p_rbtree */
     std::vector<uint32_t> u
-      = random_permutation(p_rbtree->size(), p_rgen);
+      = random_permutation(n, p_rgen);
 
+    /* will keep track of which keys remain as we move them */
+    std::map<uint32_t, uint32_t> m = map_upto(n);
+    
     /* remove keys in permutation order */
     uint32_t i = 1;
     for (uint32_t x : u) {
+      INFO(tostr("iter i: removing x from n-node tree",
+		 xtag("i", i), xtag("x", x), xtag("n", n)));
+
+      /* remove x from tracking map m also */
+      m.erase(x);
+
       // lscope.log(c_self, ": ", i, "/", n, ": remove key", xtag("x", x));
       p_rbtree->remove(x);
       // rbtree.display();
+      REQUIRE(p_rbtree->size() == n-i);
+      /* amongst other things,  this guarantees that keys in *p_rbtree
+       * appear in increasing order
+       */
       REQUIRE(p_rbtree->verify_ok());
+      /* 1. rbtree should now contain all the keys in [0..n-1],
+       *    with u[0]..u[i-1] excluded;  this is the same as the
+       *    contents of m.  
+       */
+      auto m_ix = m.begin();
+      auto m_end_ix = m.end();
+      auto visitor_fn =
+	([&m_ix, m_end_ix]
+	 (std::pair<int, double> const & contents)
+	{
+	  REQUIRE(m_ix != m_end_ix);
+	  REQUIRE(contents.first == m_ix->second);
+	  ++m_ix;
+	});
+      p_rbtree->visit_inorder(visitor_fn);
       ++i;
     }
 
+    REQUIRE(m.empty());
     REQUIRE(p_rbtree->size() == 0);
   } /*random_removes*/
 } /*namespace*/
@@ -190,19 +282,21 @@ TEST_CASE("rbtree", "[redblacktree]") {
   auto rgen = xo::random::xoshiro256(seed);
 
   /* check iteration on empty tree */
-  check_bidirectional_iterator(rbtree);
+  check_bidirectional_iterator(0, rbtree);
 
   for(uint32_t n=1; n<=1024; n*=2) {
     /* insert [0..n-1] in random order */
     random_inserts(n, &rgen, &rbtree);
     /* check iterator traverses [0..n-1] in both directions */
-    check_bidirectional_iterator(rbtree);
+    check_bidirectional_iterator(0, rbtree);
     /* verify behavior of read-only variant of operator[] */
     random_lookups(rbtree, &rgen);
     /* verify that lookups didn't disturb tree contents */
-    check_bidirectional_iterator(rbtree);
+    check_bidirectional_iterator(0, rbtree);
     /* verify update via read/write operator[] */
-    random_updates(rbtree, &rgen);
+    random_updates(10000, &rbtree, &rgen);
+    /* verify that updates changed tree contents in expected way */
+    check_bidirectional_iterator(10000, rbtree);
     /* verify behavior of read/write variant of operator[] */
     random_removes(&rgen, &rbtree);
   }
