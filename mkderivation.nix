3e98fe20616f6a6f34dbafea3162235579038b32
Roland Conybeare <rconybeare@gmail.com>
2022-05-27 15:19:53 -0400

    nix: non-functional changes to mkderivation.nix for clang13

--- mkderivation.nix
+++ mkderivation.nix
@@ -9,6 +9,11 @@ pkgs: attrs:
   with pkgs;
 
   let
+    # this sets NIX_CC etc to clang-13.0.1;
+    # omitting it picks up the default clang-11.1.0 (as of 25may2022 at least)
+    #
+    #stdenv = clang13Stdenv;
+
     # attributes below will be passed as environment variables
     # to ${codebase}/builder.sh,  after overriding at least
     #   .name, .args, .setup
@@ -27,14 +32,36 @@ pkgs: attrs:
       setup = ./stub-setup.sh;
 
       # need next two things on darwin
+      # in nixpkgs, clang -> 11.1.0 (as of 25may2022).
+      # works for building regular executables,  but llvm-cov seems to be glitched
+      # tried switching to clang_13,  however some tooling seems to insist on pulling
+      # clang11 libraries.
+      #
+      #localgcc = (if builtins.currentSystem == "aarch64-darwin" then clang_13 else gcc);
+      #localbinutils = (if builtins.currentSystem == "aarch64-darwin" then clang_13.bintools.bintools_bin else binutils-unwrapped);
+      #
       localgcc = (if builtins.currentSystem == "aarch64-darwin" then clang else gcc);
       localbinutils = (if builtins.currentSystem == "aarch64-darwin" then clang.bintools.bintools_bin else binutils-unwrapped);
 
       # all autotools builds will get these packages
       # (don't really need 'which',  this is for debugging)
       #
-      baseInputs = [ localgcc localbinutils ]
-                    ++ [ gnumake texinfo gnugrep gawk gnused gnutar xz gzip file diffutils which stdenv coreutils findutils ];
+      baseInputs = [ localgcc
+      		     localbinutils ]
+                    ++ [ gnumake
+		         texinfo
+			 gnugrep
+			 gawk
+			 gnused
+			 gnutar
+			 xz
+			 gzip
+			 file
+			 diffutils
+			 which
+			 stdenv
+			 coreutils
+			 findutils ];
       buildInputs = [];
 
       system = builtins.currentSystem;
