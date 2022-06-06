dcb01cb307023a3af99ed5eb0565eec36224034e
Roland Conybeare <rconybeare@gmail.com>
2022-06-06 12:45:18 -0400

    nix: add xterm to dev environment

--- shell.nix
+++ shell.nix
@@ -6,7 +6,7 @@ let
   mkDerivation = import ./kalman.nix pkgs;
 
   attrs = {
-    devInputs = [ emacs ripgrep less ps git gnuplot tree ];
+    devInputs = [ emacs ripgrep less ps git gnuplot tree xterm ];
     shellHook = ''
       alias ll='ls -l'
       source $setup
