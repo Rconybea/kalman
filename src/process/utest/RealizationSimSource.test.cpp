c3287d5162c1bb9c3fd68ef045e1a15f89b3f822
Roland Conybeare <rconybeare@gmail.com>
2022-06-11 22:29:54 -0400

    utest: process: add empty-simulator test

--- src/process/utest/RealizationSimSource.test.cpp
+++ src/process/utest/RealizationSimSource.test.cpp
@@ -7,14 +7,17 @@
 #include "catch2/catch.hpp"
 
 namespace xo {
+  using xo::sim::Simulator;
   //using xo::process::RealizationSimSource;
   using xo::time::Time;
   using xo::time::utc_nanos;
   //using xo::print::printer;
   //using logutil::scope;
   //using logutil::xtag;
+  using std::chrono::hours;
 
   namespace ut {
+    /* TODO: move this to time/utest/ */
     TEST_CASE("time-formatting", "[time formatting]") {
       /* TODO: unit test for time conversions */
 
@@ -37,6 +40,26 @@ namespace xo {
 #endif
     } /*TEST_CASE(time-formatting)*/
 
+    /* TODO: move this to simulator/utest/ */
+    TEST_CASE("empty-simulation", "[simulation with zero events]") {
+      constexpr char const * c_self = "TEST_CASE:empty-simulation";
+      constexpr bool c_logging_enabled = true;
+
+      /* arbitrary 'starting time' */
+      utc_nanos t0 = Time::ymd_hms_usec(20220610 /*ymd*/,
+					162905 /*hms*/,
+					123456 /*usec*/);
+
+      Simulator sim(t0);
+
+      REQUIRE(sim.is_exhausted());
+
+      utc_nanos t1 = t0 + hours(1);
+
+      sim.run_until(t1);
+
+      REQUIRE((sim.is_exhausted() || (sim.next_tm() > t1)));
+    } /*TEST_CASE(empty-simulation)*/
   } /*namespace ut*/
 } /*namespace xo*/
 
