034f1cedeb796ac0769e4db0919ade952c399391
Roland Conybeare <rconybeare@gmail.com>
2022-06-17 16:20:15 -0400

    reflect: drop \n suffix from type_name() function

--- src/reflect/demangle.hpp
+++ src/reflect/demangle.hpp
@@ -15,7 +15,8 @@ namespace xo {
     substring_as_array(std::string_view str,
 		       std::index_sequence<Idxs...> indexes)
     {
-      return std::array<char, indexes.size()+1>{str[Idxs]..., '\n'};
+      //return std::array<char, indexes.size()+1>{str[Idxs]..., '\n'};
+      return std::array<char, indexes.size()>{str[Idxs]...};
     } /*substring_as_array*/
 
     template <typename T> constexpr auto type_name_array() {
@@ -58,6 +59,33 @@ namespace xo {
       constexpr auto& value = type_name_holder<T>::value;
       return std::string_view{value.data(), value.size()};
     }
+
+#ifdef NOT_IN_USE
+    template <std::string_view const&... Strs>
+    struct join
+    {
+      // Join all strings into a single std::array of chars
+      static constexpr auto impl() noexcept
+      {
+        constexpr std::size_t len = (Strs.size() + ... + 0);
+        std::array<char, len + 1> arr{};
+        auto append = [i = 0, &arr](auto const& s) mutable {
+            for (auto c : s) arr[i++] = c;
+        };
+        (append(Strs), ...);
+        arr[len] = 0;
+        return arr;
+      }
+      // Give the joined string static storage
+      static constexpr auto arr = impl();
+      // View as a std::string_view
+      static constexpr std::string_view value {arr.data(), arr.size() - 1};
+    };
+
+    // Helper to get the value out
+    template <std::string_view const&... Strs>
+    static constexpr auto join_v = join<Strs...>::value;
+#endif
   } /*namespace reflect*/
 } /*namespace xo*/
 
