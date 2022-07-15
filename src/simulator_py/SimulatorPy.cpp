be4e084d69de7be7f9d340db4f37d2b5f6dbdba8
Roland Conybeare <rconybeare@gmail.com>
2022-07-15 16:21:12 -0400

    simulator_py: + Simulator class wrapper [wip]

--- src/simulator_py/SimulatorPy.cpp
+++ src/simulator_py/SimulatorPy.cpp
@@ -1,10 +1,20 @@
 /* @file SimulatorPy.cpp */
 
 #include <pybind11/pybind11.h>
+#include "simulator/Simulator.hpp"
+#include "refcnt/Refcounted.hpp"
+#include <pybind11/chrono.h>
+
+/* xo::ref::intrusive_ptr<T> is an intrusively-reference-counted pointer.
+ * always safe to create one from a T* p
+ * (since refcount is directly accessible from p)
+ */
+PYBIND11_DECLARE_HOLDER_TYPE(T, xo::ref::intrusive_ptr<T>, true);
 
 namespace xo {
+  namespace py = pybind11;
+
   namespace sim {
-    namespace py = pybind11;
 
     int
     xoadd(int i, int j) {
@@ -20,8 +30,20 @@ namespace xo {
 	    "A function that adds two numbers",
 	    py::arg("x"),
 	    py::arg("y"));
+
+      m.def("make_simulator",
+	    []() {
+	      return xo::sim::Simulator::make(xo::time::Time::epoch());
+	    },
+	    "create new Simulator instance");
+
+      py::class_<Simulator, xo::ref::intrusive_ptr<Simulator>>(m, "Simulator")
+	.def("start_tm", &Simulator::t0)
+	.def("is_exhausted", &Simulator::is_exhausted)
+	.def("__repr__", &Simulator::display_string);
     }
   } /*namespace sim*/
 } /*namespace xo*/
 
 /* end SimulatorPy.cpp */
+
