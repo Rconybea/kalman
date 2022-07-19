31321520b0ec904dbcf37a83983231ed3b8719fd
Roland Conybeare <rconybeare@gmail.com>
2022-07-19 13:45:08 -0400

    cosmetic: remove an OBSOLETE block

--- src/filter/KalmanFilterInputCallback.hpp
+++ src/filter/KalmanFilterInputCallback.hpp
@@ -8,20 +8,6 @@
 namespace xo {
   namespace kalman {
     using KalmanFilterInputCallback = reactor::Sink1<KalmanFilterInput>;
-
-#ifdef OBSOLETE
-    class KalmanFilterInputCallback : public reactor::Sink1<KalmanFilterInput> {
-    public:
-      // ----- inherited from Sink1<KalmanFilterInput> -----
-
-      /* notification with updated filter state */
-      virtual void notify_input(KalmanFilterInput const & input) override = 0;
-
-      /* CallbackSet invokes these on add/remove events */
-      virtual void notify_add_callback() override {}
-      virtual void notify_remove_callback() override {}
-    }; /*KalmanFilterInputCallback*/
-#endif
   } /*namespace kalman*/
 } /*namespace xo*/
 
